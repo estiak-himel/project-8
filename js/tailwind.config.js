9f4c4d7fe194e19f2df7905031edf04fabc84e73
estiak-himel <estiakhimel9@gmail.com>
2023-11-20 10:59:55 +0600

    first commit

---
+++ js/tailwind.config.js
@@ -0,0 +1,50 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      // colorPart st
+      colors: {
+          'menuColor':'rgba(29, 15, 133, 0.5)',
+          'buttonColor': '#1BBF00',
+          'overlycolor':'rgb(107, 99, 197,0.9)',
+          'aontoher1':'#C4C4C4',
+          'servcolor':'#F6F5FF',
+          'textblue' : '#141135',
+          'pcolor' : '#6C7D93',
+          'sotoColor': 'rgb(135, 218, 135)',
+          'hovernil':'#6B62C5',
+          'whitebg':'#F9F9FB'
+      },
+      // colorPart end
+      // container width st
+      maxWidth: {
+        'ContainerWidth': '1170px',
+        'bannarWidth': '1170px',
+        'choiseContainer': '1147px',
+        'ImpactContainer': '1107px',
+        'vedioContainer':'985px'
+        
+      },
+      // container width end
+      // gooogle fonts st 
+      fontFamily: {
+        'opens': ['Open Sans', 'sans-serif'],
+        'Paprika': ['Paprika', 'sans-serif'],
+        'Quicksan': ['Quicksand', 'sans-serif'],
+      
+      },
+      // gooogle fonts end
+      // bacgrond img st 
+      backgroundImage: {
+        'bannarimage':"url('images/image.jpg')",
+        'clientimge' :"url('images/bg.png')",
+        'cho0sebg':"url('images/bg2222222222.jpg')",
+        'impactBg':"url('images/impactbg.png')",
+      },
+       // bacgrond img end
+      //  service part st 
+
+      //  service part end
+
+    }
+  }
+}
